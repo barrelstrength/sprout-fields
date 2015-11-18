3cdad1e46eaea9918bdd7dbd529aa5793477e417
Selvin Ortiz <selvin@selvin.co>
2015-11-18 05:27:06 -0600

    Cleans up messy formatting and redundant comments in notes field type

--- fieldtypes/SproutFields_NotesFieldType.php
+++ fieldtypes/SproutFields_NotesFieldType.php
@@ -1,100 +1,90 @@
 <?php
 namespace Craft;
 
+/**
+ * Class SproutFields_NotesFieldType
+ *
+ * @package Craft
+ */
 class SproutFields_NotesFieldType extends BaseFieldType
 {
-    private $options = array(
-        'style' => array(
-            'infoPrimaryDocumentation' => 'Primary Information',
-            'infoSecondaryDocumentation' => 'Secondary Information',
-            'warningDocumentation' => 'Warning',
-            'dangerDocumentation' => 'Danger',
-            'highlightDocumentation' => 'Highlight'
-        ),
-        'output' => array(
-            'markdown' => 'Markdown',
-            'richText' => 'Rich Text',
-            'html' => 'HTML'
-        )
-    );
+	private $options = array(
+		'style'  => array(
+			'infoPrimaryDocumentation'   => 'Primary Information',
+			'infoSecondaryDocumentation' => 'Secondary Information',
+			'warningDocumentation'       => 'Warning',
+			'dangerDocumentation'        => 'Danger',
+			'highlightDocumentation'     => 'Highlight'
+		),
+		'output' => array(
+			'markdown' => 'Markdown',
+			'richText' => 'Rich Text',
+			'html'     => 'HTML'
+		)
+	);
 
-    /**
-     * Blocktype name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return Craft::t('Notes');
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Notes');
+	}
 
-    /**
-     * Define database column
-     *
-     * @return false
-     */
-    public function defineContentAttribute()
-    {
-        // block type doesn’t need its own column
-        // in the content table, return false
-        return false;
-    }
+	/**
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		return array(
+			'instructions' => array(AttributeType::Mixed),
+			'style'        => array(AttributeType::Mixed, 'default' => 'infoPrimaryDocumentation'),
+			'output'       => array(AttributeType::Mixed, 'default' => 'markdown')
+		);
+	}
 
-    /**
-     * Define blocktype settings
-     *
-     * @return array List of our available styles
-     */
-    protected function defineSettings()
-    {
-        return array(
-            'instructions' => array(AttributeType::Mixed),
-            'style' => array(AttributeType::Mixed, 'default' => 'infoPrimaryDocumentation'),
-            'output' => array(AttributeType::Mixed, 'default' => 'markdown')
-        );
-    }
+	/**
+	 * @return string
+	 */
+	public function getSettingsHtml()
+	{
+		$settings = $this->getSettings();
 
-    /**
-     * Display our settings
-     *
-     * @return string Returns the template which displays our settings
-     */
-    public function getSettingsHtml()
-    {
-        $settings = $this->getSettings();      
+		return craft()->templates->render(
+			'sproutfields/_fieldtypes/notes/settings', array(
+				'options'  => $this->options,
+				'settings' => $settings
+			)
+		);
+	}
 
-        return craft()->templates->render('sproutfields/_fieldtypes/notes/settings', array(
-            'options' => $this->options,
-            'settings' => $settings
-        ));
-    }
+	/**
+	 * @param string $name   Our blocktype handle
+	 * @param string $value  Always returns blank, our block
+	 *                       only styles the Instructions field
+	 *
+	 * @return string Return our blocks input template
+	 */
+	public function getInputHtml($name, $value)
+	{
+		$settings       = $this->getSettings();
+		$selectedStyle  = $settings->style;
+		$pluginSettings = craft()->plugins->getPlugin('sproutfields')->getSettings()->getAttributes();
 
-    /**
-     * Display our blocktype
-     *
-     * @param string $name  Our blocktype handle
-     * @param string $value Always returns blank, our block
-     *                       only styles the Instructions field
-     * @return string Return our blocks input template
-     */
-    public function getInputHtml($name, $value)
-    {
-        $settings = $this->getSettings();
-        $selectedStyle = $settings->style;
+		// @todo - can probably simplify this code once settings are in place
+		$selectedStyleCss = "";
+		if (isset($pluginSettings[$selectedStyle]))
+		{
+			$selectedStyleCss = str_replace("{{ name }}", $name, $pluginSettings[$selectedStyle]);
+		}
 
-        $pluginSettings = craft()->plugins->getPlugin('sproutfields')->getSettings()->getAttributes();
-
-        // @todo - can probably simplify this code once settings are in place
-        $selectedStyleCss = "";
-        if (isset($pluginSettings[$selectedStyle]))
-        {
-            $selectedStyleCss = str_replace("{{ name }}", $name, $pluginSettings[$selectedStyle]);
-        }
-
-        return craft()->templates->render('sproutfields/_fieldtypes/notes/input', array(
-            'name' => $name,
-            'settings' => $settings,
-            'selectedStyleCss' => $selectedStyleCss
-        ));
-    }
+		return craft()->templates->render(
+			'sproutfields/_fieldtypes/notes/input',
+			array(
+				'name'             => $name,
+				'settings'         => $settings,
+				'selectedStyleCss' => $selectedStyleCss
+			)
+		);
+	}
 }

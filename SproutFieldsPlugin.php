cf2687297c354a4a8f85f84d8b3957d8d2a75508
Ben Parizek <ben@barrelstrengthdesign.com>
2015-12-01 04:40:39 -0800

    Updates Release Feed URL to https

--- SproutFieldsPlugin.php
+++ SproutFieldsPlugin.php
@@ -67,7 +67,7 @@ class SproutFieldsPlugin extends BasePlugin
 	 */
 	public function getReleaseFeedUrl()
 	{
-		return 'http://sprout.barrelstrengthdesign.com/craft-plugins/fields/releases.json';
+		return 'https://sprout.barrelstrengthdesign.com/craft-plugins/fields/releases.json';
 	}
 
 	/**

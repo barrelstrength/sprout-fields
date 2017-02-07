edb255d0bae24fc0def51e7e63c083f8c8332afe
Andre Lopez <andre@barrelstrengthdesign.com>
2017-02-06 20:46:39 -0400

    Adds Notes Field Type

--- src/SproutFields.php
+++ src/SproutFields.php
@@ -7,12 +7,16 @@ use craft\events\RegisterComponentTypesEvent;
 use craft\services\Fields;
 use yii\base\Component;
 
+use barrelstrength\sproutfields\helpers\SproutFieldsInstallHelper;
+use barrelstrength\sproutfields\models\SettingsModel;
+
 use barrelstrength\sproutfields\fields\Hidden      as HiddenField;
 use barrelstrength\sproutfields\fields\Phone       as PhoneField;
 use barrelstrength\sproutfields\fields\Email       as EmailField;
 use barrelstrength\sproutfields\fields\EmailSelect as EmailSelectField;
 use barrelstrength\sproutfields\fields\Invisible   as InvisibleField;
 use barrelstrength\sproutfields\fields\Link        as LinkField;
+use barrelstrength\sproutfields\fields\Notes       as NotesField;
 use barrelstrength\sproutfields\services\PhoneService;
 
 class SproutFields extends \craft\base\Plugin
@@ -27,7 +31,7 @@ class SproutFields extends \craft\base\Plugin
 	public function init()
 	{
 		parent::init();
-		
+
 		self::$api = $this->get('api');
 
 		Event::on(Fields::class, Fields::EVENT_REGISTER_FIELD_TYPES, function(RegisterComponentTypesEvent $event) {
@@ -37,8 +41,46 @@ class SproutFields extends \craft\base\Plugin
 				$event->types[] = EmailSelectField::class;
 				$event->types[] = InvisibleField::class;
 				$event->types[] = LinkField::class;
+				$event->types[] = NotesField::class;
 			}
 		);
+	}
+
+	/**
+	 * @param string $message
+	 * @param array  $params
+	 *
+	 * @return string
+	 */
+	public static function t($message, array $params = [])
+	{
+			return Craft::t('sproutFields', $message, $params);
+	}
+
+	/**
+	 * Returns settings model with custom properties
+	 *
+	 * @return SettingsModel
+	 */
+	public function createSettingsModel()
+	{
+		return new SettingsModel();
+	}
+
+	public function settingsHtml()
+	{
+		return Craft::$app->getView()->renderTemplate(
+			'sproutfields/_cp/settings',
+			[
+				'settings' => $this->getSettings()
+			]
+		);
+	}
+
+	public function afterInstall()
+	{
+		$helper = new SproutFieldsInstallHelper();
+		$helper->installDefaultNotesStyles();
 	}
 }
 
---
+++ src/fields/Notes.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace barrelstrength\sproutfields\fields;
+
+use Craft;
+use craft\base\ElementInterface;
+use craft\base\Field;
+use craft\base\PreviewableFieldInterface;
+use yii\db\Schema;
+use craft\web\assets\redactor\RedactorAsset;
+use craft\web\assets\richtext\RichTextAsset;
+
+use barrelstrength\sproutfields\SproutFields;
+
+class Notes extends Field
+{
+
+	/**
+	 * @var text
+	 */
+	public $instructions;
+
+	/**
+	 * @var text
+	 */
+	public $style;
+
+	/**
+	 * @var bool
+	 */
+	public $hideLabel;
+
+	/**
+	 * @var text
+	 */
+	public $output;
+
+	public static function displayName(): string
+	{
+		return Craft::t('sproutFields', 'Notes');
+	}
+
+	/**
+	 * Define database column
+	 *
+	 * @return false
+	 */
+	public function defineContentAttribute()
+	{
+		// field type doesn’t need its own column
+		// in the content table, return false
+		return false;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getSettingsHtml()
+	{
+		$name             = $this->displayName();
+
+		$inputId          = Craft::$app->getView()->formatInputId($name);
+		$namespaceInputId = Craft::$app->getView()->namespaceInputId($inputId);
+
+		$view = Craft::$app->getView();
+		$view->registerAssetBundle(RedactorAsset::class);
+		$view->registerAssetBundle(RichTextAsset::class);
+
+		return Craft::$app->getView()->renderTemplate(
+			'sproutfields/_fieldtypes/notes/settings',
+			[
+				'options'  => $this->getOptions(),
+				'id'       => $namespaceInputId,
+				'name'     => $name,
+				'field'    => $this,
+			]
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getInputHtml($value, ElementInterface $element = null): string
+	{
+		$name             = $this->displayName();
+		$inputId          = Craft::$app->getView()->formatInputId($name);
+		$namespaceInputId = Craft::$app->getView()->namespaceInputId($inputId);
+		$selectedStyle    = $this->style;
+		$pluginSettings   = Craft::$app->plugins->
+												getPlugin('sproutfields')->
+												getSettings()->getAttributes();
+		$selectedStyleCss = "";
+
+		if (isset($pluginSettings[$selectedStyle]))
+		{
+			$selectedStyleCss = str_replace(
+				"{{ name }}",
+				$name,
+				$pluginSettings[$selectedStyle]
+			);
+		}
+
+		return Craft::$app->getView()->renderTemplate(
+			'sproutfields/_fieldtypes/notes/input',
+			[
+				'id'               => $namespaceInputId,
+				'name'             => $name,
+				'field'            => $this,
+				'selectedStyleCss' => $selectedStyleCss
+			]
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getElementValidationRules(): array
+	{
+		$rules = parent::getElementValidationRules();
+		$rules[] = 'validateLink';
+
+		return $rules;
+	}
+
+	/**
+	 * Validates our fields submitted value beyond the checks
+	 * that were assumed based on the content attribute.
+	 *
+	 *
+	 * @param ElementInterface $element
+	 *
+	 * @return void
+	 */
+	public function validateLink(ElementInterface $element)
+	{
+		$value = $element->getFieldValue($this->handle);
+
+		$handle  = $this->handle;
+		$name    = $this->name;
+
+		if (!SproutFields::$api->link->validate($value, $this))
+		{
+			$element->addError(
+				$this->handle,
+				SproutFields::$api->link->getErrorMessage($this)
+			);
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getTableAttributeHtml($value, ElementInterface $element)
+	{
+		$html = '';
+
+		if ($value)
+		{
+			$html = '<a href="' . $value . '" target="_blank">' . $value . '</a>';
+		}
+
+		return $html;
+	}
+
+	private function getOptions()
+	{
+		$options = [
+			'style'  => [
+				'default'                    => 'Default',
+				'infoPrimaryDocumentation'   => 'Primary Information',
+				'infoSecondaryDocumentation' => 'Secondary Information',
+				'warningDocumentation'       => 'Warning',
+				'dangerDocumentation'        => 'Danger',
+				'highlightDocumentation'     => 'Highlight'
+			],
+			'output' => [
+				'markdown' => 'Markdown',
+				'richText' => 'Rich Text',
+				'html'     => 'HTML'
+			]
+		];
+
+		return $options;
+	}
+}

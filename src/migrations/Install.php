cee8c51013046046f64d3beebb9ae7af6caf02e4
Ben Parizek <ben@barrelstrengthdesign.com>
2017-10-07 12:51:03 -0400

    Refactors Install conventions

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -2,23 +2,11 @@
 
 namespace barrelstrength\sproutfields\migrations;
 
-use Craft;
-use craft\config\DbConfig;
 use craft\db\Migration;
-use craft\models\Structure;
-use barrelstrength\sproutseo\models\Settings;
-use barrelstrength\sproutcore\migrations\sproutfields\AddressTable;
+use barrelstrength\sproutcore\migrations\sproutfields\Install as SproutCoreFieldsInstall;
 
 class Install extends Migration
 {
-	// Public Properties
-	// =========================================================================
-
-	/**
-	 * @var string The database driver to use
-	 */
-	public $driver;
-
 	// Public Methods
 	// =========================================================================
 
@@ -27,37 +15,20 @@ class Install extends Migration
 	 */
 	public function safeUp()
 	{
-		$this->createAddressTable();
+		$this->runSproutCoreInstall();
 
 		return true;
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function safeDown()
-	{
-		$sproutSeo = Craft::$app->plugins->getPlugin('sprout-seo');
-
-		if (!$sproutSeo)
-		{
-			$migration = new AddressTable();
-
-			ob_start();
-			$migration->down();
-			ob_end_clean();
-		}
 	}
 
 	// Protected Methods
 	// =========================================================================
 
-	protected function createAddressTable()
+	protected function runSproutCoreInstall()
 	{
-		$migration = new AddressTable();
+		$migration = new SproutCoreFieldsInstall();
 
 		ob_start();
-		$migration->up();
+		$migration->safeUp();
 		ob_end_clean();
 	}
 }

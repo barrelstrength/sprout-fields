ecd4d72b7336135119cbc9e9036fa1d0c69006a2
Ben Parizek <ben@barrelstrengthdesign.com>
2018-02-11 11:43:11 -0500

    Updates code syntax for Gender field

--- src/fields/Gender.php
+++ src/fields/Gender.php
@@ -10,12 +10,14 @@ use barrelstrength\sproutfields\SproutFields;
 
 class Gender extends Field
 {
-
     /**
      * @var text
      */
     public $genderOptions;
 
+    /**
+     * @return string
+     */
     public static function displayName(): string
     {
         return SproutFields::t('Gender (Sprout)');
@@ -82,32 +84,40 @@ class Gender extends Field
     {
         $options = [
             [
-                'label' => Craft::t('sprout-fields','Select...'),
+                'label' => Craft::t('sprout-fields', 'Select...'),
                 'value' => ''
             ],
             [
-                'label' => Craft::t('sprout-fields','Female'),
+                'label' => Craft::t('sprout-fields', 'Female'),
                 'value' => 'female'
             ],
             [
-                'label' => Craft::t('sprout-fields','Male'),
+                'label' => Craft::t('sprout-fields', 'Male'),
                 'value' => 'male',
             ],
             [
-                'label' => Craft::t('sprout-fields','Prefer Not to Say'),
+                'label' => Craft::t('sprout-fields', 'Prefer Not to Say'),
                 'value' => 'decline'
             ]
         ];
 
         $gender = $value ?? null;
 
-        array_push($options, ['optgroup' => Craft::t('sprout-fields','Custom')]);
+        $options[] = [
+            'optgroup' => Craft::t('sprout-fields', 'Custom')
+        ];
 
         if (!array_key_exists($gender, ['female' => 0, 'male' => 1, 'decline' => 2]) && $gender != '') {
-            array_push($options, ['label' => $gender, 'value' => $gender]);
+            $options[] = [
+                'label' => $gender,
+                'value' => $gender
+            ];
         }
 
-        array_push($options, ['label' => Craft::t('sprout-fields','Add Custom'), 'value' => 'custom']);
+        $options[] = [
+            'label' => Craft::t('sprout-fields', 'Add Custom'),
+            'value' => 'custom'
+        ];
 
         return $options;
     }

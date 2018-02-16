1844401c3265c2045a6322794f11b39235fb7bec
Andre Lopez <andrelopezd@gmail.com>
2018-02-16 18:07:07 -0400

    Adds initial migration to handle Craft2to3

---
+++ src/migrations/m180216_161522_rename_fields.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace barrelstrength\sproutfields\migrations;
+
+use craft\db\Migration;
+use barrelstrength\sproutfields\fields\EmailDropdown;
+use barrelstrength\sproutfields\fields\Notes;
+use barrelstrength\sproutfields\fields\RegularExpression;
+use barrelstrength\sproutfields\fields\Url;
+use barrelstrength\sproutfields\fields\Phone;
+use craft\db\Query;
+
+/**
+ * m180216_161522_rename_fields migration.
+ */
+class m180216_161522_rename_fields extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Notes - Update style to empty and Type
+        $notesFields = (new Query())
+            ->select(['id', 'handle', 'settings'])
+            ->from(['{{%fields}}'])
+            ->where(['type' => 'SproutFields_Notes'])
+            ->all();
+
+        foreach ($notesFields as $noteField) {
+            $settings = json_decode($noteField['settings'], true);
+            $settings['style'] = '';
+            $settingsAsJson = json_encode($settings);
+
+            $this->update('{{%fields}}', ['type' => Notes::class, 'settings' => $settingsAsJson], ['id' => $noteField['id']], [], false);
+        }
+        // end notes
+
+        // Phone - Update settings and Type
+        $phoneFields = (new Query())
+            ->select(['id', 'handle', 'settings'])
+            ->from(['{{%fields}}'])
+            ->where(['type' => 'SproutFields_Phone'])
+            ->all();
+
+        foreach ($phoneFields as $phoneField) {
+            $newSettings = [];
+            $settings = json_decode($phoneField['settings'], true);
+            $newSettings['placeholder'] = $settings['placeholder'] ?? '';
+            $newSettings['customPatternErrorMessage'] = $settings['customPatternErrorMessage'] ?? '';
+            $newSettings['country'] = 'US';
+            $newSettings['limitToSingleCountry'] = '';
+            $settingsAsJson = json_encode($newSettings);
+
+            $this->update('{{%fields}}', ['type' => Phone::class, 'settings' => $settingsAsJson], ['id' => $phoneField['id']], [], false);
+        }
+        // end Phone
+
+        // Email Select to Email dropdown
+        $this->update('{{%fields}}', [
+            'type' => EmailDropdown::class
+        ], [
+            'type' => 'SproutFields_EmailSelect'
+        ], [], false);
+
+        // SproutFields_RegularExpression
+        $this->update('{{%fields}}', [
+            'type' => RegularExpression::class
+        ], [
+            'type' => 'SproutFields_RegularExpression'
+        ], [], false);
+
+        // Link to Url
+        $this->update('{{%fields}}', [
+            'type' => Url::class
+        ], [
+            'type' => 'SproutFields_Link'
+        ], [], false);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180216_161522_rename_fields cannot be reverted.\n";
+        return false;
+    }
+}

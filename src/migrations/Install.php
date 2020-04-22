4040a0d02e499df185fc664e15b8559675172160
Ben Parizek <ben.parizek@gmail.com>
2020-04-21 22:05:21 -0400

    Adds uninstall migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * @link https://sprout.barrelstrengthdesign.com
+ * @link      https://sprout.barrelstrengthdesign.com
  * @copyright Copyright (c) Barrel Strength Design LLC
- * @license https://craftcms.github.io/license
+ * @license   https://craftcms.github.io/license
  */
 
 namespace barrelstrength\sproutfields\migrations;
 
+use barrelstrength\sproutbase\base\SproutDependencyInterface;
+use barrelstrength\sproutbase\migrations\Install as SproutBaseInstall;
 use barrelstrength\sproutbasefields\migrations\Install as SproutBaseFieldsInstall;
+use barrelstrength\sproutfields\SproutFields;
 use craft\db\Migration;
 
 class Install extends Migration
@@ -22,6 +25,36 @@ class Install extends Migration
         ob_start();
         $migration->safeUp();
         ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function safeDown(): bool
+    {
+        /** @var SproutFields $plugin */
+        $plugin = SproutFields::getInstance();
+
+        $sproutBaseFieldsInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE_FIELDS);
+        $sproutBaseInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE);
+
+        if (!$sproutBaseFieldsInUse) {
+            $migration = new SproutBaseFieldsInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
+
+        if (!$sproutBaseInUse) {
+            $migration = new SproutBaseInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
 
         return true;
     }

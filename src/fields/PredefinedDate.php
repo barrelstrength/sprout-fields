926040c96a9731aef26b1d5483a7851d431729a9
Ben Parizek <ben.parizek@gmail.com>
2020-05-21 23:11:36 -0400

    Adds support for Predefined Date field in Element Index

--- src/fields/PredefinedDate.php
+++ src/fields/PredefinedDate.php
@@ -5,6 +5,7 @@ namespace barrelstrength\sproutfields\fields;
 use Craft;
 use craft\base\ElementInterface;
 use craft\helpers\DateTimeHelper;
+use craft\i18n\Locale;
 use Exception;
 use Throwable;
 use Twig\Error\LoaderError;
@@ -82,5 +83,20 @@ class PredefinedDate extends BasePredefinedField
                 'name' => $this->handle,
                 'value' => $value
             ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTableAttributeHtml($value, ElementInterface $element): string
+    {
+        if ($value) {
+            $formatter = Craft::$app->getFormatter();
+
+            /** @var DateTime $value */
+            return '<span title="' . $formatter->asDatetime($value, Locale::LENGTH_SHORT) . '">' . $formatter->asTimestamp($value, Locale::LENGTH_FULL) . '</span>';
+        }
+
+        return '';
     }
 }
